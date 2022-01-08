841d310e43045a39b5b24ac2f24d3a859422a2b9
kkznch <kkznch710md@gmail.com>
2022-01-08 15:29:29 +0900

    たまにSuspendするサンプル作成した

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import { Suspense } from 'react';
-import { AlwaysSuspend } from './components/AlwaysSuspend';
+import {
+  Suspense,
+  useState,
+} from 'react';
+// import { AlwaysSuspend } from './components/AlwaysSuspend';
+import { SometimesSuspend } from './components/SometimesSuspend';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <h1>React App!</h1>
       <Suspense fallback={<p>Loading...</p>}>
-        <AlwaysSuspend />
+        {/* <AlwaysSuspend /> */}
+        <SometimesSuspend />
+        <button onClick={() => setCount((c) => c + 1)}>
+          {count}
+        </button>
       </Suspense>
     </div>
   );
---
+++ src/components/SometimesSuspend.tsx
@@ -0,0 +1,8 @@
+import { sleep } from '../libs/sleep';
+
+export const SometimesSuspend: React.FC = () => {
+  if (Math.random() < 0.5) {
+    throw sleep(1000);
+  }
+  return <p>Hello, world!</p>;
+};
