1250b5aac76670656f175ced13c59f30f3bd2cd8
kkznch <kkznch710md@gmail.com>
2022-01-08 16:30:03 +0900

    手動でローディングさせるようにした

--- src/components/DataLoader.tsx
+++ src/components/DataLoader.tsx
@@ -2,11 +2,19 @@ import { useState } from 'react';
 import { fetchData1 } from '../libs/api';
 
 export const DataLoader: React.VFC = () => {
+  const [loading, setLoading] = useState(false);
   const [data, setData] = useState<string | null>(null);
-  // dataがまだ無ければローディングを開始する
-  if (data === null) {
+  // ローディングフラグが立っていてdataがまだ無ければローディングを開始する
+  if (loading && data === null) {
     throw fetchData1().then(setData);
   }
   // データがあればそれを表示
-  return <div>Data is {data}</div>;
+  return (
+    <div>
+      <div>Data is {data}</div>
+      <button onClick={() => setLoading(true)}>
+        load
+      </button>
+    </div>
+  );
 };
